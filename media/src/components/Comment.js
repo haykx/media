25f3a256a891a1069d428093c2631fbabe8d1547
haykx <hayner2006@gmail.com>
2023-03-23 08:05:14 +0400

    comments progress

--- media/src/components/Comment.js
+++ media/src/components/Comment.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Like from "./Like";
 import AddComment from "./AddComment";
+import login from "./Login";
 
 
 function Comment(props) {
@@ -8,7 +9,7 @@ function Comment(props) {
     const [comment, setComment] = useState();
     const [size, setSize] = useState();
     const [display, setDisplay] = useState();
-    // const token = 'Bearer ' + localStorage.getItem('token');
+    const token = 'Bearer ' + localStorage.getItem('token');
 
     useEffect(()=>{
         setComment(props.comment);
@@ -17,7 +18,16 @@ function Comment(props) {
     }, [props.comment, props.size]);
 
     const showMore = (e) => {
-        //TODO: write fetch, etc.
+        if(!comment.replies){
+            fetch(`http://localhost:8040/api/v1/comment/${comment.id}`, {
+                headers: {
+                    Authorization: token,
+                    Accept: "application/json"
+                }
+            }).then(response => response.json())
+                .then(data => setComment(data))
+                .catch(e => console.log(e));
+        }
 
         setDisplay(!display);
     };
@@ -35,16 +45,15 @@ function Comment(props) {
                     </div>
                 </div>
                 <Like scope={'comment'} id={comment?.id} likes={comment?.likes} isLiked={comment?.liked} />
-                <button onClick={showMore}>show more</button>
+                <button style={{display: comment?.replyCount ? "flex" : "none"}} onClick={showMore}>Show {comment?.replyCount} replies</button>
+                <AddComment scope={'comment'} parent={comment?.id} />
                 <div style={{display: display ? "flex" : "none"}}>
-
+                    {comment?.replies?.map((r, j) => (
+                        <Comment comment={r} size={size}/>
+                    ))}
                 </div>
-                {/*<AddComment scope={'comment'} parent={comment?.id} />*/}
             </div>
 
-            {/*{comment?.replies?.map((r, j) => (*/}
-            {/*    <Comment comment={r} size={size-10}/>*/}
-            {/*))}*/}
 
         </div>
     );
